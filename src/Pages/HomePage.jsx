150c63a59358aafb0b6ec4ed5dab7858f541748e
asishghos <ccasish01@gmail.com>
2025-01-15 01:38:01 +0530

    fix

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div style={{ padding: '20px', textAlign: 'center' }}>
+            <h1>Welcome to My Page</h1>
+            <p>My React page.</p>
+        </div>
+    );
+};
+
+export default HomePage;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import AppRoutes from './routes.jsx';
-
+import App from './App.jsx';
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppRoutes />
+    <App />
   </React.StrictMode>
 );
