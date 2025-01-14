ea3a01b9a7c1cca868cb4494e955639b006bd488
asishghos <138640359+asishghos@users.noreply.github.com>
2025-01-15 01:30:08 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import TechNovaHomepage from './Pages/home_page.jsx';
+// import AboutUsPage from './Pages/about_us_page.jsx';
+// import ContactPage from './Pages/contact_us_page.jsx';
+// import ServicesPage from './Pages/services_page.jsx';
+// import NotFound from './Pages/not_found_page.jsx';
+import HomePage from './Pages/HomePage.jsx';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/about" element={<AboutUsPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/services" element={<ServicesPage />} /> */}
+        {/* <Route path="*" element={<NotFound />} /> Catch-all route */}
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
